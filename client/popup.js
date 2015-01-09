2daae5ed45cf8ea2e5b720950196adb3a5ea5ae7
Caroline Wong <carowong5@gmail.com>
2015-01-08 22:08:26 -0800

    add code to home

--- client/popup.js
+++ client/popup.js
@@ -9,191 +9,46 @@
  * @type {string}
  */
 
+var allProf = [];
 
-var data = { status: 200,
-  results:
-   [ { distance_mi: 9,
-       common_like_count: 0,
-       common_friend_count: 0,
-       common_likes: [],
-       common_friends: [],
-       _id: '542b4ae466087c0a451caad4',
-       bio: 'IG gottaluv_kee',
-       birth_date: '1989-10-29T00:00:00.000Z',
-       gender: 1,
-       name: 'Keanem',
-       ping_time: '2015-01-08T22:59:14.690Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 2,
-       common_like_count: 2,
-       common_friend_count: 1,
-       common_likes: [Object],
-       common_friends: [Object],
-       _id: '54adbac3b30528f876bec9b5',
-       bio: 'I hate cats.',
-       birth_date: '1987-10-07T00:00:00.000Z',
-       gender: 1,
-       name: 'Elisabeth',
-       ping_time: '2015-01-09T04:58:35.464Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 13,
-       common_like_count: 0,
-       common_friend_count: 0,
-       common_likes: [],
-       common_friends: [],
-       _id: '54ae004b8a26bae5180347c1',
-       bio: 'Scuba diver, science nerd, Tar Heel fan, and travel enthusiast. ',
-       birth_date: '1988-01-21T00:00:00.000Z',
-       gender: 1,
-       name: 'Lindley',
-       ping_time: '2015-01-09T04:47:30.085Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 2,
-       common_like_count: 0,
-       common_friend_count: 0,
-       common_likes: [],
-       common_friends: [],
-       _id: '530a299a2f88f3c943000488',
-       bio: '',
-       birth_date: '1991-04-15T00:00:00.000Z',
-       gender: 1,
-       name: 'Allison',
-       ping_time: '2015-01-09T04:58:33.989Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 2,
-       common_like_count: 1,
-       common_friend_count: 15,
-       common_likes: [Object],
-       common_friends: [Object],
-       _id: '54604c309dafc05f683f7f70',
-       bio: '',
-       birth_date: '1985-05-03T00:00:00.000Z',
-       gender: 1,
-       name: 'Francesca',
-       ping_time: '2015-01-09T04:51:26.213Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 5,
-       common_like_count: 0,
-       common_friend_count: 0,
-       common_likes: [],
-       common_friends: [],
-       _id: '5387c26e02a422085da9a93f',
-       bio: '"Anyone who tries to make brownies without butter should be shot."\n\nUVA 2012\nCurrently getting my MSc. in Biology at VCU\nI study forest ecology, love hiking and being outdoors, and also love anything related to food, cooking, and baking. ',
-       birth_date: '1990-03-26T00:00:00.000Z',
-       gender: 1,
-       name: 'Amy',
-       ping_time: '2015-01-09T04:21:04.446Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 1,
-       common_like_count: 0,
-       common_friend_count: 0,
-       common_likes: [],
-       common_friends: [],
-       _id: '537a9427f4a223753c46a2f2',
-       bio: 'I <3 thunderstorms.\r\n\r\ni\'m opting out of becoming a real grown up',
-       birth_date: '1987-12-20T00:00:00.000Z',
-       gender: 1,
-       name: 'Caroline',
-       ping_time: '2014-10-06T23:44:49.578Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 12,
-       common_like_count: 1,
-       common_friend_count: 0,
-       common_likes: [Object],
-       common_friends: [],
-       _id: '537919f5396b32c324a66f9d',
-       bio: 'I love hockey(no, roller hockey doesn\'t count), cursing, having a weird sleep schedule, and making other people laugh. Before you ask, I\'m a penguins fan and proud of it!Find what you love, and let it kill you.Snapchat: tgreg91IG: callmeTgreg ',
-       birth_date: '1991-08-15T00:00:00.000Z',
-       gender: 1,
-       name: 'Tori',
-       ping_time: '2015-01-09T04:45:15.247Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 1,
-       common_like_count: 0,
-       common_friend_count: 0,
-       common_likes: [],
-       common_friends: [],
-       _id: '52a4b7d55d3f3a7d2c000048',
-       bio: '',
-       birth_date: '1984-10-02T00:00:00.000Z',
-       gender: 1,
-       name: 'Natalie',
-       ping_time: '2015-01-09T04:54:28.066Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 10,
-       common_like_count: 0,
-       common_friend_count: 0,
-       common_likes: [],
-       common_friends: [],
-       _id: '548316351c293d2478a2349e',
-       bio: 'Instagram @erinobrien88 what do I say here .. If you really want to see the real me watch my vines ..@erin Ashley.. Tattoos make me weak...I\'m a lot of personality to handle (fair warning ) and if you\'re looking for a "swolemate" I am not your lady ha..height 7\'15" weight 672 if that matters',
-       birth_date: '1988-01-15T00:00:00.000Z',
-       gender: 1,
-       name: 'Erin',
-       ping_time: '2015-01-09T03:48:59.567Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 2,
-       common_like_count: 0,
-       common_friend_count: 0,
-       common_likes: [],
-       common_friends: [],
-       _id: '54519036c22e3969178f0c12',
-       bio: '',
-       birth_date: '1990-01-05T00:00:00.000Z',
-       gender: 1,
-       name: 'Ally',
-       ping_time: '2015-01-08T21:12:28.056Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' },
-     { distance_mi: 6,
-       common_like_count: 0,
-       common_friend_count: 2,
-       common_likes: [],
-       common_friends: [Object],
-       _id: '54ab12157deedfc90e3a68f1',
-       bio: '',
-       birth_date: '1990-04-26T00:00:00.000Z',
-       gender: 1,
-       name: 'Alice',
-       ping_time: '2015-01-09T04:47:35.687Z',
-       photos: [Object],
-       birth_date_info: 'fuzzy birthdate active, not displaying real birth_date' } ] };
-
-
-var getProfiles = {
-  requestProf: function() {
+var requestProf = function() {
     var req = new XMLHttpRequest();
-    var getUrl = "https://swiperight.azurewebsite.com/api"
+    var getUrl = "https://swiperight.azurewebsites.net/api"
     req.open("GET", getUrl);
-    req.onload = this.showProfiles.bind(this);
+    req.onload = this.storeProfiles.bind(this);
     req.send(null);
-  },
+};
 
-  showProfiles: function (e) {
-    console.log(data);
-    // var kittens = e.target.responseXML.querySelectorAll('photo');
-    // for (var i = 0; i < kittens.length; i++) {
-    //   var img = document.createElement('img');
-    //   img.src = this.constructKittenURL_(kittens[i]);
-    //   img.setAttribute('alt', kittens[i].getAttribute('title'));
-    //   document.body.appendChild(img);
-    // }
+//shift profile off queue and show
+//run when button is pressed
+var storeProfiles = function(e) {
+  console.log(e);
+  // var prof = e.target.responseXML.querySelectorAll('photo');
+  //store objects as allProf
+
+  // for (var i = 0; i < kittens.length; i++) {
+  //   var img = document.createElement('img');
+  //   img.src = this.constructKittenURL_(kittens[i]);
+  //   img.setAttribute('alt', kittens[i].getAttribute('title'));
+  //   document.body.appendChild(img);
+  // }
+};
+
+var showProf = function() {
+  if (allProf.length === 0) {
+      requestProf();
   }
+  var prof = allProf.shift();
+  // document.getElementById("profName").innerHTML(prof.name).classList.add(prof);
+  var profImgDiv = document.getElementById("profImg")
+  //profImgDiv.src = prof.photos.src;
+
 };
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  getProfiles.requestProfiles();
+  requestProf();
+  showProf();
 });
 
 // var QUERY = 'kittens';
